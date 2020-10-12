cf6d1b07a882f9f76c353f737b8f9f886d7cdeda
Mason Stallmo <mason@kaliberlabs.com>
2020-10-12 15:58:59 -0700

    Add ImageDev and ImageFloatDev that expect GPU allocated image input

--- src/popsift/s_image.cu
+++ src/popsift/s_image.cu
@@ -167,6 +167,119 @@ void Image::createTexture( )
 }
 
 /*************************************************************
+ * ImageDev
+ *************************************************************/
+
+ImageDev::ImageDev( )
+  : ImageBase( 0, 0 )
+{
+}
+
+ImageDev::ImageDev( int w, int h )
+  : ImageBase( w, h )
+{
+    allocate( w, h );
+}
+
+ImageDev::~ImageDev( )
+{
+    if( _max_w == 0 ) return;
+
+    destroyTexture( );
+    _input_image_d.freeDev( );
+}
+
+void ImageDev::load( void* input )
+{
+    cudaMemcpy(_input_image_d.data, input, _w*_h, cudaMemcpyDeviceToDevice); // assume that host Plane2D has no pitch
+}
+
+void ImageDev::resetDimensions( int w, int h )
+{
+    if( _max_w == 0 && _max_h == 0 ) {
+        _max_w = _w = w;
+        _max_h = _h = h;
+        allocate( w, h );
+        return;
+    }
+
+    if( w == _w && h == _h ) return;
+    /* everything OK, nothing to do */
+
+    _w = w;
+    _h = h;
+
+    if( w <= _max_w && h <= _max_h ) {
+        _input_image_d.resetDimensionsDev( w, h );
+
+        destroyTexture( );
+        createTexture( );
+    } else {
+        nvtxRangePushA( "reallocating host-side image memory" );
+
+        _max_w = max( w, _max_w );
+        _max_h = max( h, _max_h );
+        _input_image_d.freeDev( );
+        _input_image_d.allocDev(  _max_w, _max_h );
+        _input_image_d.resetDimensionsDev( w, h );
+
+        destroyTexture( );
+        createTexture( );
+
+        nvtxRangePop(); // "reallocating host-side image memory"
+    }
+}
+
+void ImageDev::allocate( int w, int h )
+{
+    nvtxRangePushA( "allocating host-side image memory" );
+
+    _input_image_d.allocDev( w, h );
+
+    createTexture( );
+
+    nvtxRangePop(); // "allocating host-side image memory"
+}
+
+void ImageDev::destroyTexture( )
+{
+    cudaError_t err;
+    err = cudaDestroyTextureObject( _input_image_tex );
+    POP_CUDA_FATAL_TEST( err, "Could not destroy texture object: " );
+}
+
+void ImageDev::createTexture( )
+{
+    /* initializing texture for upscaling
+     */
+    memset( &_input_image_texDesc, 0, sizeof(cudaTextureDesc) );
+    _input_image_texDesc.normalizedCoords = 1; // address 0..1 instead of 0..width/height
+    _input_image_texDesc.addressMode[0]   = cudaAddressModeClamp;
+    _input_image_texDesc.addressMode[1]   = cudaAddressModeClamp;
+    _input_image_texDesc.addressMode[2]   = cudaAddressModeClamp;
+    _input_image_texDesc.readMode         = cudaReadModeNormalizedFloat; // automatic conversion from uchar to float
+    _input_image_texDesc.filterMode       = cudaFilterModeLinear; // bilinear interpolation
+    // _input_image_texDesc.filterMode       = cudaFilterModePoint; // nearest neighbour mode
+
+    memset( &_input_image_resDesc, 0, sizeof(cudaResourceDesc) );
+    _input_image_resDesc.resType                  = cudaResourceTypePitch2D;
+    _input_image_resDesc.res.pitch2D.devPtr       = _input_image_d.data;
+    _input_image_resDesc.res.pitch2D.desc.f       = cudaChannelFormatKindUnsigned;
+    _input_image_resDesc.res.pitch2D.desc.x       = 8; // sizeof(uint8_t)*8
+    _input_image_resDesc.res.pitch2D.desc.y       = 0;
+    _input_image_resDesc.res.pitch2D.desc.z       = 0;
+    _input_image_resDesc.res.pitch2D.desc.w       = 0;
+    assert( _input_image_d.elemSize() == 1 );
+    _input_image_resDesc.res.pitch2D.pitchInBytes = _input_image_d.getPitchInBytes();
+    _input_image_resDesc.res.pitch2D.width        = _input_image_d.getCols();
+    _input_image_resDesc.res.pitch2D.height       = _input_image_d.getRows();
+
+    cudaError_t err;
+    err = cudaCreateTextureObject( &_input_image_tex, &_input_image_resDesc, &_input_image_texDesc, 0 );
+    POP_CUDA_FATAL_TEST( err, "Could not create texture object: " );
+}
+
+/*************************************************************
  * ImageFloat
  *************************************************************/
 
@@ -290,5 +403,123 @@ void ImageFloat::createTexture( )
     POP_CUDA_FATAL_TEST( err, "Could not create texture object: " );
 }
 
+/*************************************************************
+ * ImageFloatDev
+ *************************************************************/
+
+ImageFloatDev::ImageFloatDev( )
+  : ImageBase( 0, 0 )
+{
+}
+
+ImageFloatDev::ImageFloatDev( int w, int h )
+  : ImageBase( w, h )
+{
+    allocate( w, h );
+}
+
+ImageFloatDev::~ImageFloatDev( )
+{
+    if( _max_w == 0 ) return;
+
+    destroyTexture( );
+    _input_image_d.freeDev( );
+}
+
+void ImageFloatDev::load( void* input )
+{
+    /* The host memcpy may seem like a really stupid idea, but _input_image_h
+     * is in CUDA-allocated pinned host memory, which makes the H2D copy
+     * much faster.
+     */
+//    memcpy( _input_image_h.data, input, _w*_h*sizeof(float) ); // assume that host Plane2D has no pitch
+//    _input_image_h.memcpyToDevice( _input_image_d );
+    cudaMemcpy(_input_image_d.data, input, _w*_h*sizeof(float), cudaMemcpyDeviceToDevice);
+}
+
+void ImageFloatDev::resetDimensions( int w, int h )
+{
+    if( _max_w == 0 && _max_h == 0 ) {
+        _max_w = _w = w;
+        _max_h = _h = h;
+        allocate( w, h );
+        return;
+    }
+
+    if( w == _w && h == _h ) return;
+    /* everything OK, nothing to do */
+
+    _w = w;
+    _h = h;
+
+    if( w <= _max_w && h <= _max_h ) {
+        _input_image_d.resetDimensionsDev( w, h );
+
+        destroyTexture( );
+        createTexture( );
+    } else {
+        nvtxRangePushA( "reallocating host-side image memory" );
+
+        _max_w = max( w, _max_w );
+        _max_h = max( h, _max_h );
+        _input_image_d.freeDev( );
+        _input_image_d.allocDev(  _max_w, _max_h );
+        _input_image_d.resetDimensionsDev( w, h );
+
+        destroyTexture( );
+        createTexture( );
+
+        nvtxRangePop(); // "reallocating host-side image memory"
+    }
+}
+
+void ImageFloatDev::allocate( int w, int h )
+{
+    nvtxRangePushA( "allocating host-side image memory" );
+
+    _input_image_d.allocDev( w, h );
+
+    createTexture( );
+
+    nvtxRangePop(); // "allocating host-side image memory"
+}
+
+void ImageFloatDev::destroyTexture( )
+{
+    cudaError_t err;
+    err = cudaDestroyTextureObject( _input_image_tex );
+    POP_CUDA_FATAL_TEST( err, "Could not destroy texture object: " );
+}
+
+void ImageFloatDev::createTexture( )
+{
+    /* initializing texture for upscaling
+     */
+    memset( &_input_image_texDesc, 0, sizeof(cudaTextureDesc) );
+    _input_image_texDesc.normalizedCoords = 1; // address 0..1 instead of 0..width/height
+    _input_image_texDesc.addressMode[0]   = cudaAddressModeClamp;
+    _input_image_texDesc.addressMode[1]   = cudaAddressModeClamp;
+    _input_image_texDesc.addressMode[2]   = cudaAddressModeClamp;
+    _input_image_texDesc.readMode         = cudaReadModeElementType; // no conversion
+    _input_image_texDesc.filterMode       = cudaFilterModeLinear; // bilinear interpolation
+    // _input_image_texDesc.filterMode       = cudaFilterModePoint; // nearest neighbour mode
+
+    memset( &_input_image_resDesc, 0, sizeof(cudaResourceDesc) );
+    _input_image_resDesc.resType                  = cudaResourceTypePitch2D;
+    _input_image_resDesc.res.pitch2D.devPtr       = _input_image_d.data;
+    _input_image_resDesc.res.pitch2D.desc.f       = cudaChannelFormatKindFloat;
+    _input_image_resDesc.res.pitch2D.desc.x       = 32; // sizeof(float)*8
+    _input_image_resDesc.res.pitch2D.desc.y       = 0;
+    _input_image_resDesc.res.pitch2D.desc.z       = 0;
+    _input_image_resDesc.res.pitch2D.desc.w       = 0;
+    assert( _input_image_d.elemSize() == 4 );
+    _input_image_resDesc.res.pitch2D.pitchInBytes = _input_image_d.getPitchInBytes();
+    _input_image_resDesc.res.pitch2D.width        = _input_image_d.getCols();
+    _input_image_resDesc.res.pitch2D.height       = _input_image_d.getRows();
+
+    cudaError_t err;
+    err = cudaCreateTextureObject( &_input_image_tex, &_input_image_resDesc, &_input_image_texDesc, 0 );
+    POP_CUDA_FATAL_TEST( err, "Could not create texture object: " );
+}
 } // namespace popsift
 
